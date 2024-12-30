1ccde75486b2e786a623cdccc506fd9c830f75fd
Alex Seeley <alexseeley134@gmail.com>
2024-12-29 22:56:04 -0500

    Group api image loading and useEffect import

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,59 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
+const imageArray = [
+  { displayName: "Laptop", url: "https://picsum.photos/id/1/200/300" },
+
+  { displayName: "Landscape", url: "https://picsum.photos/id/10/2500/1667" },
+];
+
 async function fetchBlob(url) {
   const response = await fetch(url);
   return response.blob();
 }
 
 function App() {
-  const [imageSourceUrl, setImageSourceUrl] = useState("");
+  const [imageSourceUrlArray, setImageSourceUrlArray] = useState([]);
+  const [isBusy, setIsBusy] = useState(true);
 
-  async function getBlobAndAssign(imageURL) {
-    const image = await fetchBlob(imageURL);
-    setImageSourceUrl(URL.createObjectURL(image));
+  async function fetchAllBlobsAndAssign(data) {
+    const allData = await Promise.all(
+      // this is assuming the 'data' in question is an array of objects, objects with a displayName and url
+      data.map((item) => fetchBlob(item.url))
+    );
+
+    const allBlobUrlStrings = allData.map((item) => URL.createObjectURL(item));
+
+    let newData = [];
+
+    for (let i = 0; i < data.length; i++) {
+      newData[i] = {
+        displayName: data[i].displayName,
+        url: allBlobUrlStrings[i],
+      };
+    }
+    setImageSourceUrlArray(newData);
+    setIsBusy(false);
   }
 
-  const getBlobAndAssignUrl = async (imageURL) => {
-    const image = await fetchBlob(imageURL);
-    setImageSourceUrl(URL.createObjectURL(image));
-    console.log("Things worked?");
-  };
-
   useEffect(() => {
-    getBlobAndAssign("https://picsum.photos/id/1/200/300");
+    fetchAllBlobsAndAssign(imageArray);
   }, []);
 
-  return (
-    <>
-      <img src={imageSourceUrl} alt="" />
-    </>
-  );
+  if (!isBusy) {
+    return (
+      <>
+        <div className="card-component">
+          <h1>{imageSourceUrlArray[0].displayName}</h1>
+          <img src={imageSourceUrlArray[0].url} alt="" />
+        </div>
+        <div className="card-component">
+          <h1>{imageSourceUrlArray[1].displayName}</h1>
+          <img src={imageSourceUrlArray[1].url} alt="" />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
